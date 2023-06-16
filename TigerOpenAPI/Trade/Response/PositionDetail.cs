fa4fec64e30d72ccff3f100775a5760bac395ba8
liutongping <liutongping@itiger.com>
2023-06-16 13:29:14 +0800

    增加持仓字段

--- TigerOpenAPI/Trade/Response/PositionDetail.cs
+++ TigerOpenAPI/Trade/Response/PositionDetail.cs
@@ -12,15 +12,25 @@ namespace TigerOpenAPI.Trade.Response
     public long Position { get; set; }
 
     [JsonProperty(PropertyName = "averageCost")]
-    public double AverageCost { get; set; }
+    public Double AverageCost { get; set; }
+    [JsonProperty(PropertyName = "averageCostByAverage")]
+    public Double AverageCostByAverage { get; set; }
     [JsonProperty(PropertyName = "marketValue")]
     public Double MarketValue { get; set; } = Double.NaN;
     [JsonProperty(PropertyName = "latestPrice")]
     public Double LatestPrice { get; set; } = Double.NaN;
     [JsonProperty(PropertyName = "realizedPnl")]
     public Double RealizedPnl { get; set; } = Double.NaN;
+    [JsonProperty(PropertyName = "realizedPnlByAverage")]
+    public Double RealizedPnlByAverage { get; set; } = Double.NaN;
     [JsonProperty(PropertyName = "unrealizedPnl")]
     public Double UnrealizedPnl { get; set; } = Double.NaN;
+    [JsonProperty(PropertyName = "unrealizedPnlByAverage")]
+    public Double UnrealizedPnlByAverage { get; set; } = Double.NaN;
+    [JsonProperty(PropertyName = "unrealizedPnlPercent")]
+    public Double UnrealizedPnlPercent { get; set; } = Double.NaN;
+    [JsonProperty(PropertyName = "unrealizedPnlPercentByAverage")]
+    public Double UnrealizedPnlPercentByAverage { get; set; } = Double.NaN;
 
     [JsonProperty(PropertyName = "salable")]
     public Int32 Salable { get; set; }
