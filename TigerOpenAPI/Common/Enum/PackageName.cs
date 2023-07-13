ee5cec0c9a539b9991ea454c869a9e512366a8d8
liutongping <liutongping@itiger.com>
2023-07-13 17:31:53 +0800

    支持返回OTC标的

---
+++ TigerOpenAPI/Common/Enum/PackageName.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace TigerOpenAPI.Common.Enum
+{
+  public enum PackageName
+  {
+    none,
+
+    /**
+     * popular stock
+     */
+    package_popular,
+
+    /**
+     * Chinese stocks
+     */
+    package_china,
+
+    /**
+     * ETF
+     */
+    package_etf,
+
+    /**
+     * S&P 500
+     */
+    package_indices_INX,
+
+    /**
+     * Russell 2000
+     */
+    package_indices_RUT,
+
+    /**
+     * hang seng index
+     */
+    package_HSI,
+
+    /**
+     * China HSCEI
+     */
+    package_HSCEI,
+
+    /**
+     * Red Chip Index
+     */
+    package_HSCCI
+  }
+}
