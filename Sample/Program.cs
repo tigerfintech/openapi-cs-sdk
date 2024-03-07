4157fbf891e04c5edf7f75cdf5c5e27567c2023d
liutongping <liutongping@itiger.com>
2024-03-07 17:53:36 +0800

    增加价格合理判断的util方法

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text.Json.Nodes;
 using Newtonsoft.Json;
+using NUnit.Framework;
 using Sample;
 using TigerOpenAPI.Common;
 using TigerOpenAPI.Common.Enum;
@@ -26,6 +27,10 @@ class Program
     TigerConfig.LogDir = "/data0/logs/tiger-openapi-cs";
     ApiLogger.DebugEnabled = true;
     ApiLogger.Info("start");
+
+    //StockPriceTests stockPriceTests = new StockPriceTests();
+    //stockPriceTests.TestStockPrice();
+    //TestStockPrice();
 
     // tiger config
     TigerConfig config = new TigerConfig()
@@ -978,7 +983,7 @@ class Program
         "20200821144442583", // tradeClient.GetDefaultAccount,
         contract,
         ActionType.BUY,
-        1, 120.0
+        1, 120.0, 0.002
       )
     };
     // set other parameter
@@ -1840,5 +1845,35 @@ class Program
       ApiMethodName = QuoteApiService.USER_LICENSE
     };
     return await quoteClient.ExecuteAsync(request);
+  }
+
+  public static void TestStockPrice()
+  {
+    string content = "[{\"begin\":\"0\",\"end\":\"0.25\",\"type\":\"CLOSED\",\"tickSize\":0.001},"
+        + "{\"begin\":\"0.25\",\"end\":\"0.5\",\"type\":\"OPEN_CLOSED\",\"tickSize\":0.005},"
+        + "{\"begin\":\"0.5\",\"end\":\"10\",\"type\":\"OPEN_CLOSED\",\"tickSize\":0.01},"
+        + "{\"begin\":\"10\",\"end\":\"20\",\"type\":\"OPEN_CLOSED\",\"tickSize\":0.02},"
+        + "{\"begin\":\"20\",\"end\":\"100\",\"type\":\"OPEN_CLOSED\",\"tickSize\":0.05},"
+        + "{\"begin\":\"100\",\"end\":\"200\",\"type\":\"OPEN_CLOSED\",\"tickSize\":0.1},"
+        + "{\"begin\":\"200\",\"end\":\"500\",\"type\":\"OPEN_CLOSED\",\"tickSize\":0.2},"
+        + "{\"begin\":\"500\",\"end\":\"1000\",\"type\":\"OPEN_CLOSED\",\"tickSize\":0.5},"
+        + "{\"begin\":\"1000\",\"end\":\"2000\",\"type\":\"OPEN_CLOSED\",\"tickSize\":1.0},"
+        + "{\"begin\":\"2000\",\"end\":\"5000\",\"type\":\"OPEN_CLOSED\",\"tickSize\":2.0},"
+        + "{\"begin\":\"5000\",\"end\":\"Infinity\",\"type\":\"OPEN\",\"tickSize\":5.0}]";
+
+    List<TickSizeItem>? tickSizeItemList = JsonConvert.DeserializeObject<List<TickSizeItem>>(content);
+
+    ApiLogger.Info("input: 0.34m output expect: 10.34m, result: " + (10.34m == StockPriceUtil.FixPriceByTickSize(10.34m, tickSizeItemList)));
+    ApiLogger.Info("input: 0.35m output expect: 10.34m, result: " + (10.34m == StockPriceUtil.FixPriceByTickSize(10.35m, tickSizeItemList)));
+    ApiLogger.Info("input: 0.36m output expect: 10.36m, result: " + (10.36m == StockPriceUtil.FixPriceByTickSize(10.36m, tickSizeItemList)));
+    ApiLogger.Info("input: 0.35m output expect: 10.36m, result: " + (10.36m == StockPriceUtil.FixPriceByTickSize(10.35m, tickSizeItemList, true)));
+
+    ApiLogger.Info("input: 0.34m output expect: true, result: " + StockPriceUtil.MatchTickSize(10.34m, tickSizeItemList));
+    ApiLogger.Info("input: 0.35m output expect: false, result: " + StockPriceUtil.MatchTickSize(10.35m, tickSizeItemList));
+    ApiLogger.Info("input: 0.36m output expect: true, result: " + StockPriceUtil.MatchTickSize(10.36m, tickSizeItemList));
+
+    ApiLogger.Info("input: 0.360m output expect: true, result: " + StockPriceUtil.MatchTickSize(10.360m, tickSizeItemList));
+    ApiLogger.Info("input: 0.361m output expect: fasle, result: " + StockPriceUtil.MatchTickSize(10.361m, tickSizeItemList));
+
   }
 }
